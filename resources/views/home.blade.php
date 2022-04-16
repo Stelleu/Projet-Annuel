6708cd7648624b79537490865980e6cee8017fc4
Agustin Gomez <agustin@smspartner.fr>
2022-04-16 17:34:41 +0200

    update enviroment - route home page maked

---
+++ resources/views/home.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css" />
+        
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>EASYSCOOTER</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+        <div class="container1">
+            <div class="block1">
+                <div class=block2>
+                    <H1>HOLA</H1>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
