5ea3685ea1049d0650e75eb80e85ce48ad0aad6b
Estelle <nkumba.estelle@gmail.com>
2022-05-10 20:37:48 +0200

    Test insertion BDD
    uniquement les nombres qui rentre

--- AppC/ConnectBDD.c
+++ AppC/ConnectBDD.c
@@ -41,51 +41,52 @@ void initPreparedStatements(MYSQL*connect,double *t,char *city , double *tmn, do
 
     stmtInsertJSON = mysql_stmt_init(connect);
     if (mysql_stmt_prepare(stmtInsertJSON, INSERT_JSON,(strlen(INSERT_JSON)))){
-        printf("cc");
         fprintf(stderr, " mysql_stmt_prepare(), INSERT failed\n");
         fprintf(stderr, " %s\n", mysql_stmt_error(stmtInsertJSON));
         exit(0);
+
     }param_count = mysql_stmt_param_count(stmtInsertJSON);
     printf("there are %ld parameters in the insert statements\n", param_count);
     //(,,tmn,,w,i)
     MYSQL_BIND    bind[6];
 
+
     //BIND lier les valeurs de données de paramètre aux tampons
-    bind[0].buffer_type= MYSQL_TYPE_FLOAT;
-    bind[0].buffer= (char *)&t;
+    bind[0].buffer_type= MYSQL_TYPE_DOUBLE;
+    bind[0].buffer= (double *)t;
     bind[0].is_null= 0;
     bind[0].length= 0;
-
-/* STRING PARAM */
+/* STRING PARAM
     bind[1].buffer_type= MYSQL_TYPE_STRING;
     bind[1].buffer= (char *)city;
     bind[1].buffer_length= strlen(city);
     bind[1].is_null= 0;
     bind[1].length= &city;
+    */
 
-    bind[2].buffer_type= MYSQL_TYPE_FLOAT;
-    bind[2].buffer= (char *)&tmn;
+    bind[1].buffer_type= MYSQL_TYPE_DOUBLE;
+    bind[1].buffer= (double *)tmn;
+    bind[1].is_null= &is_null;
+    bind[1].length= 0;
+
+    bind[2].buffer_type= MYSQL_TYPE_DOUBLE;
+    bind[2].buffer= (double *)tmx;
     bind[2].is_null= &is_null;
     bind[2].length= 0;
 
-    bind[3].buffer_type= MYSQL_TYPE_FLOAT;
-    bind[3].buffer= (char *)&tmx;
-    bind[3].is_null= &is_null;
-    bind[3].length= 0;
-
-/* STRING PARAM */
-    bind[4].buffer_type= MYSQL_TYPE_STRING;
-    bind[4].buffer= (char *)w;
-    bind[4].buffer_length= strlen(w);
-    bind[4].is_null= 0;
-    bind[4].length= &w;
-
+///* STRING PARAM */
+//    bind[3].buffer_type= MYSQL_TYPE_STRING;
+//    bind[3].buffer= (char *)w;
+//    bind[3].buffer_length= strlen(w);
+//    bind[3].is_null= 0;
+//    bind[3].length= &w;
+////
     /* STRING PARAM */
-    bind[5].buffer_type= MYSQL_TYPE_STRING;
-    bind[5].buffer= (char *)i;
-    bind[5].buffer_length= strlen(i);
-    bind[5].is_null= 0;
-    bind[5].length= &i;
+    bind[3].buffer_type= MYSQL_TYPE_STRING;
+    bind[3].buffer= (char *)i;
+    bind[3].buffer_length= strlen(i);
+    bind[3].is_null= 0;
+    bind[3].length= &i;
 
 
     if (mysql_stmt_bind_param(stmtInsertJSON, bind))
--- AppC/Struct.h
+++ AppC/Struct.h
@@ -5,7 +5,8 @@
 #ifndef APPC_STRUCT_H
 #define APPC_STRUCT_H
 
-#define INSERT_JSON "INSERT INTO cities(temperature,city_name,degree_min,degree_max,weather,icon) VALUES ('%f,%s,%f,%f,%s,%s')"
+//#define INSERT_JSON "INSERT INTO cities(temperature,city_name,degree_min,degree_max,weather,icon) VALUES (?,?,?,?,?,?)"
+#define INSERT_JSON "INSERT INTO cities(temperature,degree_min,degree_max,icon) VALUES (?,?,?,?)"
 
 #define host "185.166.188.1"
 #define user "u486471496_admeasyscooter"
