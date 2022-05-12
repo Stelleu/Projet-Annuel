144e70d8645fc2ca797f34144ffb01216317af04
agustin <agustinngomezdeltoro@gmail.com>
2022-05-12 17:18:37 +0200

    add contact .php

---
+++ FRONT/contact.php
@@ -0,0 +1,73 @@
+<?php
+include("include/app.php");
+include ("include/menu.php"); 
+?>
+
+     <!-- ====== Contact End ====== -->  
+<section class="block1contact" id="home">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="ud-hero-content wow fadeInUp" data-wow-delay=".2s">
+              <h1 class="contacttitle">
+                EASYSCOOTER
+              </h1>
+              
+            </div>
+
+          </div>
+        <div class="formuler">
+      <div class="ud-contact-form-wrapper wow fadeInUp"
+          data-wow-delay=".2s">
+          <h3 class="ud-contact-form-title">Contactez-nous</h3>
+          <form class="ud-contact-form">
+            <div class="ud-form-group">
+              <label for="fullName">Nom et prenom*</label>
+              <input
+                type="text"
+                name="fullName"
+                placeholder="Mathieu Dupont"
+              />
+            </div>
+            <div class="ud-form-group">
+              <label for="email">E-mail*</label>
+              <input
+                type="email"
+                name="email"
+                placeholder="example@gmail.com"
+              />
+            </div>
+            <div class="ud-form-group">
+              <label for="phone">Telephone*</label>
+              <input
+                type="text"
+                name="phone"
+                placeholder="+33 6 40 80 21 90"
+              />
+            </div>
+            <div class="ud-form-group">
+              <label for="message">Message*</label>
+              <textarea
+                name="message"
+                rows="1"
+                placeholder="Écrivez votre message ici"
+              ></textarea>
+            </div>
+            <div class="ud-form-group mb-0">
+              <button type="submit" class="ud-main-btn">
+                Envoyer Message
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>  
+</div>
+</div>
+</section>       
+       <!-- ====== Contact End ====== -->  
+              
+     
+   
