040b537ff087fa62e7ccabbe449d815f4b79b6b2
agustin <agustinngomezdeltoro@gmail.com>
2022-05-12 15:26:54 +0200

    html to php - add include - update homepage

---
+++ FRONT/include/menu.php
@@ -0,0 +1,49 @@
+<!-- ====== Header Start ====== -->
+<header class="ud-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <nav class="navbar navbar-expand-lg">
+              <a class="navbar-brand" href="index.html">
+                <img src="assets/images/logo/logo.svg" alt="Logo">
+              </a>
+              <button class="navbar-toggler">
+                <span class="toggler-icon"> </span>
+                <span class="toggler-icon"> </span>
+                <span class="toggler-icon"> </span>
+              </button>
+
+              <div class="navbar-collapse">
+                <ul id="nav" class="navbar-nav mx-auto">
+                  <li class="nav-item">
+                    <a class="ud-menu-scroll" href="#Commentçamarche">Comment ça marche?</a>
+                  </li>
+
+                  <li class="nav-item">
+                    <a class="ud-menu-scroll" href="#about">Aide</a>
+                  </li> 
+
+                  <li class="nav-item">
+                    <a class="ud-menu-scroll" href="/FRONT/contact.html">Contact</a>
+                  </li>
+
+
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+
+              <div class="navbar-btn d-none d-sm-inline-block">
+                <a href="/FRONT/login.html" class="ud-main-btn ud-login-btn">
+                  Connexion
+                </a>
+                <a class="ud-main-btn ud-white-btn" href="/FRONT/singup.html" >
+                  S'inscrire
+                </a>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- ====== Header End ====== -->
