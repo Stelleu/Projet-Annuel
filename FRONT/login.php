8387329da1ed8055b3757e6b2252de7c1b26f811
agustin <agustinngomezdeltoro@gmail.com>
2022-05-12 15:51:01 +0200

    add login php

---
+++ FRONT/login.php
@@ -0,0 +1,81 @@
+<?php
+
+include("include/app.php");
+include ("include/menu.php");
+
+?>
+
+<!-- ====== Hero Start ====== -->
+<section class="block1contact" id="home">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="ud-hero-content wow fadeInUp" data-wow-delay=".2s">
+          <h1 class="logintitle">
+            Connexion - Espace client
+          </h1>
+          <!-- ====== Login Start ====== -->
+    <section class="ud-login">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="ud-login-wrapper">
+              <div class="ud-login-logo">
+                <img src="assets/images/logo/logo-black.svg" alt="logo" />
+              </div>
+              <form class="ud-login-form">
+                <div class="ud-form-group">
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                  />
+                </div>
+                <div class="ud-form-group">
+                  <input
+                    type="password"
+                    name="mot de passe"
+                    placeholder="*********"
+                  />
+                </div>
+                <div class="ud-form-group">
+                  <button type="submit" class="ud-main-btn w-100">Login</button>
+                </div>
+              </form>
+
+              <!---<div class="ud-socials-connect">
+                <p>Connect With</p>
+
+                <ul>
+                  <li>
+                    <a href="javascript:void(0)" class="facebook">
+                      <i class="lni lni-facebook-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)" class="twitter">
+                      <i class="lni lni-twitter-filled"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="javascript:void(0)" class="google">
+                      <i class="lni lni-google"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>--->
+
+              <a class="forget-pass" href="javascript:void(0)">
+                Mot de passe oublié ? 
+              </a>
+              <p class="signup-option">
+                Pas encore membre? <a href="/FRONT/singup.html"> S'inscrire </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</section>
+    <!-- ====== Login End ====== -->
+        
